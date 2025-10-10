5284f59d8552d4c72d84e61673f7a2f3e274aa7f
Muhammed Shoaib <muhammedshoaib06@gmail.com>
2025-10-10 12:05:48 +0530

    animation update

--- src/component/InputField.jsx
+++ src/component/InputField.jsx
@@ -113,7 +113,6 @@ function InputField() {
       x: 100,
       opacity: 0,
       duration: 1,
-      rotate: 5,
       ease: "sine.out",
     })
       .from(
@@ -284,7 +283,7 @@ function InputField() {
               className={`bg-gradient-to-r ${task.completed
                 ? `${TaskColor1[index % TaskColor1.length].start} ${TaskColor1[index % TaskColor1.length].end}`
                 : `${TaskColor[index % TaskColor.length].start} ${TaskColor[index % TaskColor.length].end}`
-                } flex flex-col gap-2 rounded-xl shadow-xl shadow-slate-400 p-6`}
+                } flex flex-col gap-2 rounded-xl shadow-lg shadow-slate-300 p-6`}
             >
               <div className="flex justify-between">
                 <div className="flex flex-col gap-3">
